b20a05f561ab6f7e59e1b28ed048ebf458aac324
aleok <alexpro820@gmail.com>
2018-09-02 15:43:08 +0200

    Update

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 minetest.register_on_chat_message(function(name, message)
-	if name != minetest.check_player_privs(name, "interact") and (string.match(message, "exit") or string.match(message, "library") or string.match(message, "out") or string.match(message, "build")) then
+	if (not minetest.check_player_privs(name, "interact")) and
+	(string.match(message, "exit") or string.match(message, "library") or string.match(message, "out") or string.match(message, "build")) then
 		minetest.chat_send_player(name, "Please read the rules and find the keyword to exit this place!")
 	end
 end)
